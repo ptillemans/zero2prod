3e4a9d412d65b30fc274840e51f98f0c8534ae0b
ptillemans <pti@snamellit.com>
2021-12-30 19:57:55 +0100

    add docker deploy support

--- src/main.rs
+++ src/main.rs
@@ -10,11 +10,14 @@ async fn main() -> std::io::Result<()> {
     init_subscriber(subscriber);
 
     let configuration = configuration::get_configuration().expect("Failed to read configuration.");
-    let address = format!("127.0.0.1:{}", configuration.application_port);
-    let listener = TcpListener::bind(address)
-        .expect("Could not bind to port 8000. Is the port already in use?");
-    let db_pool = PgPool::connect(&configuration.database.connection_string())
-        .await
+    let address = format!(
+        "{}:{}",
+        configuration.application.host, configuration.application.port,
+    );
+    tracing::info!("Starting server on  address {}", address);
+    let listener =
+        TcpListener::bind(address).expect("Could not bind to port. Is the port already in use?");
+    let db_pool = PgPool::connect_lazy(&configuration.database.connection_string())
         .expect("Failed to connect to postgres database");
     run(listener, db_pool)?.await
 }
