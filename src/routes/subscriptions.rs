02d09fe0498824a01de2d6b37b98ad23ebd843bd
ptillemans <pti@snamellit.com>
2022-01-01 17:33:47 +0100

    start with pbt

--- src/routes/subscriptions.rs
+++ src/routes/subscriptions.rs
@@ -1,3 +1,4 @@
+use crate::domain::{NewSubscriber, SubscriberEmail, SubscriberName};
 use actix_web::{web, HttpResponse};
 use chrono::Utc;
 use sqlx::PgPool;
@@ -17,8 +18,18 @@ pub struct Subscription {
         subscriber_email = %form.email
     )
 )]
+
 pub async fn subscribe(form: web::Form<Subscription>, pool: web::Data<PgPool>) -> HttpResponse {
-    match insert_subscriber(&pool, &form).await {
+    let name = match SubscriberName::parse(form.0.name) {
+        Ok(s) => s,
+        Err(_) => return HttpResponse::BadRequest().finish(),
+    };
+    let email = match SubscriberEmail::parse(form.0.email) {
+        Ok(email) => email,
+        Err(_) => return HttpResponse::BadRequest().finish(),
+    };
+    let new_subscriber = NewSubscriber { email, name };
+    match insert_subscriber(&pool, &new_subscriber).await {
         Ok(_) => {
             tracing::info!("Saving new subscriber details in the database.");
             HttpResponse::Ok().finish()
@@ -36,16 +47,16 @@ pub async fn subscribe(form: web::Form<Subscription>, pool: web::Data<PgPool>) -
 )]
 pub async fn insert_subscriber(
     pool: &PgPool,
-    subscription: &Subscription,
+    subscription: &NewSubscriber,
 ) -> Result<(), sqlx::Error> {
     sqlx::query!(
         r#"
-                INSERT INTO subscriptions (id, email, name, subscribed_at)
-                VALUES( $1 , $2, $3, $4);
-                "#,
+        INSERT INTO subscriptions (id, email, name, subscribed_at)
+        VALUES( $1 , $2, $3, $4);
+        "#,
         Uuid::new_v4(),
-        subscription.email,
-        subscription.name,
+        subscription.email.as_ref(),
+        subscription.name.as_ref(),
         Utc::now()
     )
     .execute(pool)
